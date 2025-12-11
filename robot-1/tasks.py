ef95ebcb372f2ce392b232d3f28fec795e7f1602
Aryan <iamaryan.ag@gmail.com>
2025-12-11 18:47:46 +0530

    Implemented opening website

--- robot-1/tasks.py
+++ robot-1/tasks.py
@@ -1,6 +1,18 @@
 from robocorp.tasks import task
+from robocorp import browser
 
 @task
-def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+def order_robots_from_RobotSpareBin():
+    """
+    Orders robots from RobotSpareBin Industries Inc.
+    Saves the order HTML receipt as a PDF file.
+    Saves the screenshot of the ordered robot.
+    Embeds the screenshot of the robot to the PDF receipt.
+    Creates ZIP archive of the receipts and the images.
+    """
+    open_robot_order_website()
+
+
+def open_robot_order_website():
+    order_page = browser.page()
+    order_page.goto("https://robotsparebinindustries.com/#/robot-order")
